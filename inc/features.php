76719599152217e71284fbcbfef18883e55847ce
raulshr <rahul.sharma@zinreet.com>
2023-02-22 18:32:41 +0530

    Update features.php

--- inc/features.php
+++ inc/features.php
@@ -1,4 +1,4 @@
-<section class="features pb-5" id="features">
+<section class="features" id="features">
     <div class="container">
       <div class="section-heading text-center">
         <h2 class="mb-4">Cover the Repair of Your Car</h2>
@@ -10,32 +10,32 @@
           <div class="container-fluid">
             <div class="row">
               <div class="col-xl-3 mb-lg-5 mb-md-5 mb-0 col-lg-6 col-md-6 text-center">
-                <div class="feature-item h-100">
+                <div class="feature-item h-100 text-left">
                   <img src="<?php echo SITEURL;?>img/user.png" alt="Icon used to show a user" />
                   <h3>30 Years of Experience</h3>
-                  <p>Our well-established professionals  are the best in the business. We’ll have your repairs done promptly and efficiently. Customer Satisfaction Guaranteed!</p>
+                  <p class="text-left">Our well-established professionals  are the best in the business. We’ll have your repairs done promptly and efficiently. Customer Satisfaction Guaranteed!</p>
                 </div>
               </div>
               <div class="col-xl-3 mb-lg-5 mb-md-5 mb-0 col-lg-6 col-md-6 text-center">
-                <div class="feature-item h-100">
+                <div class="feature-item h-100 text-left">
                   <img src="<?php echo SITEURL;?>img/phone.png" alt="Icon used to show a phone" />
                   <h3>Unlimited Service Calls</h3>
-                  <p>When the inevitable breakdown happens, our  highly trained and recommended service agents are available 24/7 to help you through each step! There is no limit to the amount of claims you can make. </p>
+                  <p class="text-left">When the inevitable breakdown happens, our  highly trained and recommended service agents are available 24/7 to help you through each step! There is no limit to the amount of claims you can make. </p>
                 </div>
               </div>
 
               <div class="col-xl-3 mb-lg-5 mb-md-5 mb-0 col-lg-6 col-md-6 text-center">
-                <div class="feature-item h-100">
+                <div class="feature-item h-100 text-left">
                   <img src="<?php echo SITEURL;?>img/shield.png" alt="Icon used to show a shield" />
                   <h3>Protection Guarantee</h3>
-                  <p>If your covered part cannot be repaired, we will replace it.*</p>
+                  <p class="text-left">If your covered part cannot be repaired, we will replace it.*</p>
                 </div>
               </div>
               <div class="col-xl-3 mb-lg-5 mb-md-5 mb-0 col-lg-6 col-md-6 text-center">
-                <div class="feature-item h-100">
+                <div class="feature-item h-100 text-left">
                   <img src="<?php echo SITEURL;?>img/cash-pay.png" alt="Icon used to show money" />
                   <h3>Payment Plans</h3>
-                  <p>At Select Auto Protect - we understand that auto repairs can be unexpected and costly. For this reason we offer various payment plans for our customers. </p>
+                  <p class="text-left">At Select Auto Protect - we understand that auto repairs can be unexpected and costly. For this reason we offer various payment plans for our customers. </p>
                 </div>
               </div>
         <!-- <p class="cover-repair">* Subject to the terms, conditions, and limitations set forth in your <a href="docx/selec-auto.pdf" target="_blank"><strong>service agreement</strong></a>.</p> -->
